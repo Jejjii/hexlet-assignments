88dcd817aee4d89b9a3f8f1f750c0046ed4afb3e
@hexlet/cli <support@hexlet.io>
2023-05-21 19:19:19 +0700

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -1,9 +1,27 @@
 package exercise;
 
+import java.util.stream.Collectors;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 
 // BEGIN
+public class App {
+    public static boolean scrabble(char[] symbols, String word) {
 
+        List <Character> charList = new ArrayList<>();
+        for( char symbol: symbols) {
+            charList.add(symbol);
+        }
+        List<Character> list = word.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.toList());
+
+        if(list.containsAll(charList)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
 //END
