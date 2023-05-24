1988dcef9df15e3ad3d329421f66d2ad34be0a47
@hexlet/cli <support@hexlet.io>
2023-05-24 09:59:34 +0700

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -3,20 +3,27 @@ package exercise;
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.stream.Collectors;
 // BEGIN
 public class App {
-    public static boolean scrabble(char[] symbols, String word) {
+    public static boolean scrabble(String tiles, String word) {
+        char[] tileChars = tiles.toLowerCase().toCharArray();
+        char[] wordChars = word.toLowerCase().toCharArray();
 
-        List <Character> charList = new ArrayList<>();
-        for( char symbol: symbols) {
-            charList.add(symbol);
+        int[] tileCounts = new int[26];
+
+        for (char tileChar : tileChars) {
+            tileCounts[tileChar - 'a']++;
         }
-        List<Character> list = word.chars()
-                .mapToObj(c -> (char) c)
-                .collect(Collectors.toList());
 
-        return list.contains(charList);
+        for (char wordChar : wordChars) {
+            if (tileCounts[wordChar - 'a'] <= 0) {
+                return false;
+            }
+            tileCounts[wordChar - 'a']--;
+        }
+
+        return true;
     }
 }
 //END
