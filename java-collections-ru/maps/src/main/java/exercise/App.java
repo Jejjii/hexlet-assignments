87682697e63522e5402b277e5ac5c5fea96abe16
@hexlet/cli <support@hexlet.io>
2023-05-26 13:02:54 +0700

    submit java-collections-ru/maps

---
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -0,0 +1,32 @@
+package exercise;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class App {
+
+    public static Map<String, Integer> getWordCount(String sentence) {
+        Map <String, Integer> wordCounts = new HashMap<>();
+        String[] words = sentence.split(" ");
+        for (String word : words) {
+            if(wordCounts.containsKey(word)) {
+                wordCounts.put(word, wordCounts.get(word) + 1); // 2 параметр это значение ключа, он вернет сначала 1
+                // и добавит еще +1, в значение
+            } else {
+                wordCounts.put(word, 1); //если слова нет, то значению ключа(word) присвоит 1
+            }
+        }
+        return wordCounts;
+    }
+
+    public static String toString (Map<String,Integer> wordCounts) {
+        StringBuilder strB = new StringBuilder("{");
+        for(Map.Entry<String, Integer> entry :wordCounts.entrySet()) {
+            strB.append(entry.getKey()).append(" ").append(entry.getValue()).append(", ");
+        }
+        strB.delete(strB.length() - 2, strB.length());
+        strB.append("}");
+        return strB.toString();
+    }
+}
+
