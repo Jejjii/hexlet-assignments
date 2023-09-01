c7f86609e7b0e3ffbe496812fcc7d747646e29b2
@hexlet/cli <support@hexlet.io>
2023-09-01 15:08:47 +0700

    submit java-web-ru/cookies

---
+++ java-web-ru/cookies/src/main/java/exercise/util/NamedRoutes.java
@@ -0,0 +1,18 @@
+package exercise.util;
+
+public class NamedRoutes {
+
+    public static String rootPath() {
+        return "/";
+    }
+
+    public static String addItemPath() {
+        return "/cart-items";
+    }
+
+    public static String clearCartPath() {
+        return "/cart-items/clean";
+    }
+
+
+}
